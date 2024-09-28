322b5d317521afc012d2f6ca95923a8a5db0b0cb
Chandrureka <chanvel96@gmail.com>
2024-09-28 09:02:57 +0530

    fixed some auto pipeline

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import React from "react";
 const Layout = ({children})=>{
     return(
         <div>
-            <header>hey this is my website</header>
+            <header>hey this is my websitee</header>
             <main>{children}</main>
             <footer>@2024 copyright</footer>
         </div>
