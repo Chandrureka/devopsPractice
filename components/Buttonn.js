9957ad00d1f95431c8a70126b247b8578773ea70
Chandrureka <chanvel96@gmail.com>
2024-09-30 15:53:47 +0530

    fixed redirect issue

--- components/Buttonn.js
+++ components/Buttonn.js
@@ -8,7 +8,7 @@ const SignInButton = () => {
   console.log('Status:', status);
   const handleSignIn = () => {
     // Redirect to the custom sign-in page
-    signIn('credentials', { callbackUrl: 'https://52.7.190.93:3000/auth/signin' });
+    signIn('credentials', { callbackUrl: 'http://52.7.190.93:3000' });
   };
 
 
